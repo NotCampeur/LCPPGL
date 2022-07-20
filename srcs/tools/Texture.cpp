1f9bde184dcc78325abfbb88bd4765d47e23215f
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-07-20 15:04:58 +0200

    ✨✅ Add feature to lcppgl::tools::Texture. It is now possible to work with images. Test can be found in test.cpp.

--- srcs/tools/Texture.cpp
+++ srcs/tools/Texture.cpp
@@ -6,22 +6,40 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:13:39 by ldutriez          #+#    #+#             */
-/*   Updated: 2021/12/07 14:34:20 by ldutriez         ###   ########.fr       */
+/*   Updated: 2022/07/20 14:33:21 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Texture.hpp"
 
-lcppgl::tools::Texture::Texture(Context & context, SDL_Surface *surface, bool free_surface)
-: _context(context), _texture(NULL)
+lcppgl::tools::Texture::Texture(Context &context, SDL_Surface *surface, bool free_surface)
+	: _context(context), _texture(NULL)
 {
-	if(surface == NULL)
+	if (surface == NULL)
 		throw std::runtime_error("Can not create texture from NULL surface");
 	_texture = SDL_CreateTextureFromSurface(_context.renderer(), surface);
 	if (_texture == NULL)
 		throw std::runtime_error(std::string("Can not create texture from surface: ") + SDL_GetError());
+	_width = surface->w;
+	_height = surface->h;
 	if (free_surface == true)
 		SDL_FreeSurface(surface);
+}
+
+lcppgl::tools::Texture::Texture(Context &context, const std::string &path)
+	: _context(context)
+{
+	SDL_Surface *img;
+
+	img = IMG_Load(path.c_str());
+	if (img == NULL)
+		throw std::invalid_argument(IMG_GetError());
+	_width = img->w;
+	_height = img->h;
+	_texture = SDL_CreateTextureFromSurface(lcppgl::Application::instance().context().renderer(), img);
+	if (_texture == NULL)
+		throw std::invalid_argument(SDL_GetError());
+	SDL_FreeSurface(img);
 }
 
 lcppgl::tools::Texture::~Texture(void)
@@ -30,9 +48,18 @@ lcppgl::tools::Texture::~Texture(void)
 		SDL_DestroyTexture(_texture);
 }
 
-void
-lcppgl::tools::Texture::render(const Rectangle *src, const Rectangle *dst)
+void lcppgl::tools::Texture::render(const Rectangle *src, const Rectangle *dst)
 {
 	if (SDL_RenderCopy(_context.renderer(), _texture, (const SDL_Rect *)src, (const SDL_Rect *)dst) == -1)
 		throw std::runtime_error(std::string("SDL_RenderCopy: ") + SDL_GetError());
+}
+
+int lcppgl::tools::Texture::width(void) const
+{
+	return _width;
+}
+
+int lcppgl::tools::Texture::height(void) const
+{
+	return _height;
 }
--- srcs/tools/Texture.hpp
+++ srcs/tools/Texture.hpp
@@ -6,14 +6,14 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:03:08 by ldutriez          #+#    #+#             */
-/*   Updated: 2021/12/07 14:33:31 by ldutriez         ###   ########.fr       */
+/*   Updated: 2022/07/20 14:19:49 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LCPPGL_TEXTURE_HPP
-# define LCPPGL_TEXTURE_HPP
+#define LCPPGL_TEXTURE_HPP
 
-# include "lcppgl.hpp"
+#include "lcppgl.hpp"
 
 /**
  * @brief Texture class
@@ -28,39 +28,54 @@ namespace lcppgl
 	namespace tools
 	{
 		class Rectangle;
-		
+
 		class Texture
 		{
-			private:
-				Context		&_context;
-				SDL_Texture *_texture;
-				
-			public:
+		private:
+			Context &_context;
+			SDL_Texture *_texture;
+			int _width;
+			int _height;
 
-				/**
-				 * @brief Construct a new Texture object for the given context via the given surface.
-				 * 
-				 * @param context The context to use.
-				 * @param surface The surface to transform into a texture.
-				 * @param free_surface If true, the surface will be freed after the texture creation with SDL_FreeSurface.
-				 * 
-				 * @throw std::runtime_error If the texture creation failed.,
-				 * @throw std::runtime_error If the surface is null.
-				 */
-				Texture(Context & context, SDL_Surface *surface, bool free_surface = true);
-				~Texture();
+		public:
+			/**
+			 * @brief Construct a new Texture object for the given context via the given surface.
+			 *
+			 * @param context The context to use.
+			 * @param surface The surface to transform into a texture.
+			 * @param free_surface If true, the surface will be freed after the texture creation with SDL_FreeSurface.
+			 *
+			 * @throw std::runtime_error If the texture creation failed.,
+			 * @throw std::runtime_error If the surface is null.
+			 */
+			Texture(Context &context, SDL_Surface *surface, bool free_surface = true);
 
-				/**
-				 * @brief Print the texture to the context's renderer.
-				 * 
-				 * @param src The dimensions of the texture to print. NULL to use the entire texture.
-				 * @param dst The dimensions of the destination to print the texture. NULL to use the entire context's renderer.
-				 * 
-				 * @details The texture will be stretched to fill the dst rectangle.
-				 * 
-				 * @throw std::runtime_error If SDL_RenderCopy failed.
-				 */
-				void	render(const Rectangle *src, const Rectangle *dst);
+			/**
+			 * @brief Construct a new Texture object from an image file.
+			 * 
+			 * @param context The context link to the texture that will be use to render.
+			 * @param path Path to the image file.
+			 * 
+			 * @throw std::runtime_error If the texture creation failed.
+			 * @throw std::runtime_error If the surface creation fail.
+			 */
+			Texture(Context &context, const std::string &path);
+			~Texture();
+
+			/**
+			 * @brief Print the texture to the context's renderer.
+			 *
+			 * @param src The dimensions of the texture to print. NULL to use the entire texture.
+			 * @param dst The dimensions of the destination to print the texture. NULL to use the entire context's renderer.
+			 *
+			 * @details The texture will be stretched to fill the dst rectangle.
+			 *
+			 * @throw std::runtime_error If SDL_RenderCopy failed.
+			 */
+			void render(const Rectangle *src, const Rectangle *dst);
+
+			int width(void) const;
+			int height(void) const;
 		};
 	}
 }
